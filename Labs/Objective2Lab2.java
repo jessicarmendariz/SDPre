df95cf342b2a5799d59e45f5a62216c92a362865
jessicarmendariz <jessica.n.armendariz@gmail.com>
2021-07-14 01:09:56 -0600

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Jessica Armendariz";
+    int age = 29;
+    String state = "Pennsylvania";
+    System.out.println("Name: " + name + " " + "Age: " + age + " " + "State: " + state);
+  }
+}
